322cf1e8022eec75586bdc663e815456f14f1b8a
yael oushky <yaeli7702@gmail.com>
2021-11-03 18:06:19 +0200

    change

--- js/app.controller.js
+++ js/app.controller.js
@@ -47,6 +47,7 @@ function onGetUserPos() {
             console.log('err!!!', err);
         })
 }
+
 function onPanTo() {
     console.log('Panning the Map');
     mapService.panTo(35.6895, 139.6917);
--- js/services/map.service.js
+++ js/services/map.service.js
@@ -22,6 +22,37 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
         })
 }
 
+
+function initMap() {
+    const myLatlng = { lat: -25.363, lng: 131.044 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatlng,
+    });
+    // Create the initial InfoWindow.
+    let infoWindow = new google.maps.InfoWindow({
+      content: "Click the map to get Lat/Lng!",
+      position: myLatlng,
+    });
+  
+    infoWindow.open(map);
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+      // Close the current InfoWindow.
+      infoWindow.close();
+      // Create a new InfoWindow.
+      infoWindow = new google.maps.InfoWindow({
+        position: mapsMouseEvent.latLng,
+      });
+      infoWindow.setContent(
+        JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+      );
+      infoWindow.open(map);
+    });
+  }
+
+
+
 function addMarker(loc) {
     var marker = new google.maps.Marker({
         position: loc,
