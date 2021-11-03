a86a6119b6ea3be7c3ae348ce61d830e6c03a2c5
yael oushky <yaeli7702@gmail.com>
2021-11-03 13:55:47 +0200

    secend commit

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,8 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = `AIzaSyCFnIgowG2QtJGmDQEiZIFnIBO8WTGwiOQ
+    `; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
